f96d6befcfd94f1267c904a2999c62202c598b83
Arkeshak <arkeshak2003@gmail.com>
2026-01-04 13:55:29 +0530

    Initial Android project setup - Login

---
+++ app/src/main/java/com/example/localhomeservicebookingapp/ui/customer/CustomerHomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.localhomeservicebookingapp.ui.customer
+
+class CustomerHomeActivity {
+}
---
+++ app/src/main/java/com/example/localhomeservicebookingapp/ui/worker/WorkerHomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.localhomeservicebookingapp.ui.worker
+
+class WorkerHomeActivity {
+}
